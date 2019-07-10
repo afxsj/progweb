42b363b91e91048e9aebdd5a670c103af7bdacfb
Alexsandro Fernandes <alexfersj33@gmail.com>
2019-07-10 12:21:17 -0300

    php

--- php/html_ex03.php
+++ php/html_ex03.php
@@ -1,12 +1,26 @@
-<?php
-if(isset($_GET["submit"])){
-    $pontos=$_GET["pontos"];
-    $recompensa="aaa";
-    //if com else
-    if($pontos< 50){
-      $menor="Sem direito a recompensas";
-    }
+<?php 
+if(isset($_POST["submit"])){
+    $pontos= $_POST["pontos"];
+    $recompensa= "";
+
+    if($pontos>=50 and $pontos<= 100){
+        $recompensa="descontos em produtos ";
+
+    }elseif ($pontos>=101 and $pontos <=400) {
+        $recompensa="Milhas em dobro";
+   
+    }elseif ($pontos>=401 and $pontos <=700) {
+        $recompensa="Pontos em dobro para compras em dólar";
+   
+    
+    }else 
+        $recompensa="5 vezes mais pontos nas compras em dolar";
+   
+}
+
+
 ?>
+
 
 
 
@@ -17,19 +31,60 @@ if(isset($_GET["submit"])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>]<div id="formulario">
-    <form method="post">
-            <label for="valor">Valor solicitado</label>
-            <input type="text" name="valor" placeholder="Informe o valor "><br>
-            <label for="parcelas">Parcelado em</label>
+    <title>Ex 03</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script>
+    $(document).ready(function(){
+        $("[type=range]").change(function(){
+            var novovalor=$(this).val();
+            $("#intervalo").text(novovalor);
+        });
+    });
+    </script>
 
-        <select name="parcelas">
-            <option value="12">12 parcelas</option>
-            <option value="24">24 parcelas</option>
-            <option value="36">36 parcelas</option>
-        </select><br>
+    <div id="formulario"</div>
+        <form action="html_ex03.php" method="POST">
+       
+      <label for="pontos">informe seus pontos: </label>
+      <input type="range" value="<?php echo (isset($_POST ["submit"])? $pontos:"50")?>" min="50" max="1000" name="pontos">
+      <span id="intervalo"><?php echo (isset($_POST["submit"])? $pontos:"50")?></span>
+      <input type="submit" name="submit"  value="Simular"> 
+  
+    </form>
+    </div>
+
+    <?php
+    ?>
+
+
+</div>
+<?php
+    if(isset($_POST["submit"])){
+        ?>
+        <h1>Seus pontos são</h1>
+        <?php
+        echo $pontos;
+        ?>
+      
+   
+    
+    <div id="resultado">
+    <h1>Sua recompensa é....</h1>
+    <?php
+    echo $recompensa;
+    ?>
+
+
+   
+  
+    <?php
+    }
+    ?>
+
+
+
+</head>
+<body>
     
 </body>
 </html>
