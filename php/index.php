ad137dd01575ec49b45bbdf92998c0fbe9f94ac6
Alexsandro Fernandes <alexfersj33@gmail.com>
2019-07-12 11:23:28 -0300

    php

---
+++ php/index.php
@@ -0,0 +1,62 @@
+<?php
+if(isset($_POST["submit"])){
+    $valor= $_POST["valor"];
+
+  
+}
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Aividade externa</title>
+</head>
+<body>
+
+  <div id="formulario">
+  <form action="index.php" method="POST">
+
+  <h2>A raiz quadrada  de</h2>
+  <input type="text" name="valor" value="" placeholder="Valor" />
+  
+  
+
+  <input type="submit" name="submit" value="Calcular">
+ </form>
+  
+  
+  </div>
+   <div id="resultado">
+   <?php
+   if(isset($_POST["submit"])){
+
+   ?>
+   
+   
+    <?php
+   echo sqrt($valor); // 3
+   //echo sqrt($valor);
+   }
+
+   ?>
+
+   <h3>A Raiz é</h3>
+
+ <?php
+if($valor % 2 == 0){
+    echo "par";
+} else {
+    echo "impar";
+}
+?>
+
+</div>
+ 
+    
+</body>
+</html>
